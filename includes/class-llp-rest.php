a3dd13a3337de4684d21686d17bcd4747f75d248
zKarlz <167244155+zKarlz@users.noreply.github.com>
2025-08-19 23:00:40 +0300

    Avoid duplicate storage instance in REST finalize

--- includes/class-llp-rest.php
+++ includes/class-llp-rest.php
@@ -116,8 +116,6 @@ class REST {
             'out_composite'=> $final_dir . 'composite.png',
             'out_thumb'    => $final_dir . 'thumb.jpg',
         ]);
-
-        $storage = Storage::instance();
         return rest_ensure_response([
             'asset_id'      => $asset_id,
             'original_url'  => $storage->asset_url($asset_id, 'original.png'),
